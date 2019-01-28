56880e22cdf7a7c7180904acc218914b90cefc36
Lucas Fiedler <post@lucasfiedler.de>
2019-01-28 21:50:16 +0100

    Revert "Initial commit"
    This reverts commit e5f6d32bf7fd3a13c423a0999a88908d316c8467.

--- skript_aufgabe_4-10_liste_produkt.pl
+++
@@ -1,9 +0,0 @@
-produkt([F|R],Produkt):-
-    number(F),
-    produkt(R,Produkt1),
-    Produkt is Produkt1 * F.
-produkt([],1).
-produkt(F|R,Produkt):-
-    not(number(F)),
-    produkt(R,Produkt).
-
